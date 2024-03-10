edfc5c3373014f6c6f2dadffb374c70c75f93317
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2024-03-10 12:28:21 +0530

    categories linked :link:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,33 @@
-import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
+
+// import ModalProvider from '@/providers/modal-provider'
+// import ToastProvider from '@/providers/toast-provider'
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
 import "./globals.css";
 
 const font = Urbanist({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Store",
-  description: "Store",
+  description: "Store - The place for all your purchases.",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        {/* <ToastProvider />
+        <ModalProvider /> */}
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
